83b433693513863721ac47363e0cc294875a6ee7
katpoon <katpoon@users.noreply.github.com>
2014-05-05 12:37:39 -0700

    Update backend-svr.js
    Add console log on when request is received

--- backend-svr.js
+++ backend-svr.js
@@ -4,10 +4,9 @@ const http = require('http');
 
 var server=http.createServer(function(request, response) {
 
-//    for (var item in request.headers) {
-//        console.log(item + ":" + request.headers[item]);
-//    }
-
+    var date = new Date();
+    
+    console.log('receive request at ' + date.toDateString() + ' ' + date.toTimeString() + '\n')
     response.writeHead(200, {"Content-Type":"application/json"});
     response.end(JSON.stringify(request.headers));
     
