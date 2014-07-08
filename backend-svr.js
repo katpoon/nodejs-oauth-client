06f3d7da4f1d6b38381543e04f8d3a8def74661c
katpoon <katpoon@users.noreply.github.com>
2014-07-08 16:00:25 -0400

    Update backend-svr.js
    Use logic to determine the ip address, and add some debug code

--- backend-svr.js
+++ backend-svr.js
@@ -1,15 +1,28 @@
 'use strict';
 
 const http = require('http');
+const os = require('os');
 
 var server=http.createServer(function(request, response) {
 
     var date = new Date();
     
-    console.log('receive request at ' + date.toDateString() + ' ' + date.toTimeString() + '\n')
+    console.log('receive request, ' + request.url + ', at ' + date.toDateString() + ' ' + date.toTimeString() + '\n')
     response.writeHead(200, {"Content-Type":"application/json"});
     response.end(JSON.stringify(request.headers));
     
 }).listen(8088);
+
+var localipv4addr = '';
+
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+   var alias = 0;
+   ifaces[dev].forEach(function(details) {
+       if (details.family=='IPv4' && dev == 'eth0') {
+            localipv4addr = details.address;
+       }
+   });
+}
 
 console.log('fake backend server running on port 8088.');
